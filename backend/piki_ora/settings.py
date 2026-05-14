7647ea3df62359d0688ff4a49b30056adc8cae90
sifaz <sifazhang.nz@gmail.com>
2026-05-14 19:31:12 +1200

    add vercel deployment config

--- backend/piki_ora/settings.py
+++ backend/piki_ora/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,6 +106,7 @@ else:
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Password validation
