276096f1fcce66faecf303b5695fd83e93971d59
sifaz <sifazhang.nz@gmail.com>
2026-05-14 19:42:47 +1200

    fix axios baseURL for production

--- frontend/src/api/adminAxios.js
+++ frontend/src/api/adminAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "/api/";
 
 // 请求拦截器：自动附加 access token
 axios.interceptors.request.use((config) => {
